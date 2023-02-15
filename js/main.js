77c542fe46ba203df7969e136d05e01ff5c527f2
LucasLePera <lucas02lepera@gmail.com>
2023-02-15 20:31:05 -0300

    precios y filtro

--- js/main.js
+++ js/main.js
@@ -2,6 +2,45 @@
 let usuario = prompt("Ingresa tu usuario");
 alert(`Bienvenido, ${usuario}. Este es el mejor sitio para fans de F1.`);
 
+
+let voluntadCompra = prompt("¿Deseas comprar?");
+if (voluntadCompra.toUpperCase() === "SI") {
+
+    let Merch = [
+        { id: 1, nombre: "Redbull", categoria: "Buzo", precio: 85 },
+        { id: 2, nombre: "Alpha Tauri", categoria: "Buzo", precio: 70 },
+        { id: 3, nombre: "Ferrari", categoria: "Buzo", precio: 80 },
+    ];
+    let filtrados;
+
+    let filtro = prompt("¿Por qué deseas fitrar?");
+
+    if (filtro === "precio") {
+        let precio = parseInt(prompt("Ingresa precio máximo (usd)"));
+
+        filtrados = Merch.filter((item) => item.precio > precio);
+    } else if (filtro === "categoria") {
+        let categoria = prompt("¿Qué quieres comprar?");
+
+        filtrados = Merch.filter((item) => item.categoria === categoria);
+    }
+
+    if (filtrados.length > 0) {
+        filtrados.forEach((item) => {
+            let mensaje = `
+       Id: ${item.id}
+       Nombre: ${item.nombre}
+       `;
+            alert(mensaje);
+        });
+    } else {
+        alert("De momento solo tenemos buzos");
+    }
+}
+else if (voluntadCompra.toUpperCase() === "NO") {
+    alert("Bien, disfruta de las noticias");
+}
+
 // consulta sobre notificaciones
 let notificacion = prompt("¿Deseas recibir mails con noticias?");
 
@@ -46,104 +85,5 @@ if (consultaCalcularNumero.toUpperCase() === "NO") {
     else if (nacimiento <= 31) {
         alert(`${usuario}, tu dorsal es ${numeroDorsal}`)
     }
-    if (numeroDorsal === 1) {
-        alert(`${datoCurioso} Max Verstappen`)
-    }
-    else if (numeroDorsal === 2) {
-        alert(`${datoCurioso} Logan Sargeant`)
-    }
-    else if (numeroDorsal === 4) {
-        alert(`${datoCurioso} Lando Norris`)
-    }
-    else if (numeroDorsal === 10) {
-        alert(`${datoCurioso} Pierre Gasly`)
-    }
-    else if (numeroDorsal === 11) {
-        alert(`${datoCurioso} Sergio Pérez`)
-    }
-    else if (numeroDorsal === 14) {
-        alert(`${datoCurioso} Fernando Alonso`)
-    }
-    else if (numeroDorsal === 16) {
-        alert(`${datoCurioso} Charles Leclerc`)
-    }
-    else if (numeroDorsal === 18) {
-        alert(`${datoCurioso} Lance Stroll`)
-    }
-    else if (numeroDorsal === 20) {
-        alert(`${datoCurioso} Kevin Magnussen`)
-    }
-    else if (dorsal === 21) {
-        alert(`${datoCurioso} Nyck de Vries`)
-    }
-    else if (numeroDorsal === 22) {
-        alert(`${datoCurioso} Yuki Tsunoda`)
-    }
-    else if (numeroDorsal === 23) {
-        alert(`${datoCurioso} Alex Albon`)
-    }
-    else if (numeroDorsal === 24) {
-        alert(`${datoCurioso} Guanyu Zhou`)
-    }
-    else if (numeroDorsal === 27) {
-        alert(`${datoCurioso} Nico Hulkenberg`)
-    }
-    else if (numeroDorsal === 31) {
-        alert(`${datoCurioso} Esteban Ocon`)
-    }
-    else if (numeroDorsal === 44) {
-        alert(`${datoCurioso} Lewis Hamilton`)
-    }
-    else if (numeroDorsal === 55) {
-        alert(`${datoCurioso} Carlos Sainz`)
-    }
-    else if (numeroDorsal === 63) {
-        alert(`${datoCurioso} George Russell`)
-    }
-    else if (numeroDorsal === 77) {
-        alert(`${datoCurioso} Valtteri Bottas`)
-    }
-    else if (numeroDorsal === 81) {
-        alert(`${datoCurioso} Oscar Piastri`)
-    }else{
-        alert(`Intenta nuevamente`)
-    }
-}
 
-let voluntadCompra = prompt("¿Deseas comprar?");
-if (voluntadCompra.toUpperCase() === "SI") {
-
-let Merch = [
-    { id: 1, nombre: "Redbull", categoria: "Buzo", precio: 85 },
-    { id: 2, nombre: "Alpha Tauri", categoria: "Buzo", precio: 70 },
-    { id: 3, nombre: "Ferrari", categoria: "Buzo", precio: 80 },
-  ];
-  let filtrados;
-  
-  let filtro = prompt("¿Por qué deseas fitrar?");
-  
-  if (filtro === "precio") {
-    let precio = parseInt(prompt("Ingresa precio máximo (usd)"));
-  
-    filtrados = Merch.filter((item) => item.precio > precio);
-  } else if (filtro === "categoria") {
-    let categoria = prompt("¿Qué quieres comprar?");
-  
-    filtrados = Merch.filter((item) => item.categoria === categoria);
-  }
-
-  if (filtrados.length > 0) {
-    filtrados.forEach((item) => {
-      let mensaje = `
-       Id: ${item.id}
-       Nombre: ${item.nombre}
-       `;
-      alert(mensaje);
-    });
-  } else {
-    alert("De momento solo tenemos buzos");
-  }
-}
-if (voluntadCompra.toUpperCase() === "NO") {
-    alert("Bien, disfruta de las noticias");
 }
